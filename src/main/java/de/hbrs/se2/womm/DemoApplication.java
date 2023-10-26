27188e2f76108664892100a519fb68b66085c3e8
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-10-26 12:19:42 +0200

    adding spring-boot, folder-structure VL, sonar

---
+++ src/main/java/de/hbrs/se2/womm/DemoApplication.java
@@ -0,0 +1,13 @@
+package de.hbrs.se2.womm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
