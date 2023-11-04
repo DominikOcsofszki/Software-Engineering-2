291ca2a2f826b5c64ead4ef17793625f60c42b2d
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-04 11:16:53 +0100

    style: example view

--- src/main/java/de/hbrs/se2/womm/views/XXX.java
+++ src/main/java/de/hbrs/se2/womm/views/XXX.java
@@ -1,2 +1,16 @@
-package de.hbrs.se2.womm.views;public class XXX {
+package de.hbrs.se2.womm.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "XXX")
+@PageTitle("XXX")
+public class XXX extends VerticalLayout {
+    public XXX() {
+        H1 h1 = new H1("XXX");
+        add(new H1("XXX"));
+        add(h1);
+    }
 }
