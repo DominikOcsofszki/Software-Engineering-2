d3fe5cf2ccf66a3545ed6e1a3b9084fbec0502f1
Jan Kölzer <jan.koelzer@smail.inf.h-brs.de>
2023-11-05 13:13:58 +0100

    feat(Nutzer): Datenbank Tabelle Nutzer als Nutzer.java hinzugefügt.

---
+++ src/main/java/de/hbrs/se2/womm/entities/Nutzer.java
@@ -0,0 +1,40 @@
+package de.hbrs.se2.womm.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "nutzer")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Nutzer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "nutzer_id")
+    private Integer nutzerId;
+
+    @Column(name = "nutzer_name", nullable = false)
+    private String nutzerName;
+
+    @Column(name = "nutzer_passwort", nullable = false)
+    private String nutzerPasswort;
+
+    @Column(name = "nutzer_mail", nullable = false)
+    private String nutzerMail;
+
+    @Column(name = "nutzer_aktiv", nullable = false)
+    private Boolean nutzerAktiv;
+
+    @Column(name = "nutzer_ort", nullable = false)
+    private String nutzerOrt;
+
+    @Lob
+    @Column(name = "nutzer_profilbild")
+    private byte[] nutzerProfilbild;
+
+}
+
