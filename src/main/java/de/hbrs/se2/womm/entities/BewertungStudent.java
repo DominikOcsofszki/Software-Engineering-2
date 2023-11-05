67d899259bf72aee56d468a10afdc3be159dbe4a
Jan Kölzer <jan.koelzer@smail.inf.h-brs.de>
2023-11-05 13:16:01 +0100

    feat(Bewertung): Datenbank Tabelle BewertungStudent und BewertungUnternehmen als BewertungStudent.java und BewertungUnternehmen.java hinzugefügt.

---
+++ src/main/java/de/hbrs/se2/womm/entities/BewertungStudent.java
@@ -0,0 +1,35 @@
+package de.hbrs.se2.womm.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "bewertung_student")
+@Getter
+@Setter
+@NoArgsConstructor
+public class BewertungStudent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bewertung_student_id")
+    private Integer bewertungStudentId;
+
+    @Column(name = "bewertung_student_wertung")
+    private Integer bewertungStudentWertung;
+
+    @Column(name = "bewertung_student_text", columnDefinition = "text")
+    private String bewertungStudentText;
+
+    @ManyToOne
+    @JoinColumn(name = "unternehmen_id")
+    private Unternehmen unternehmen;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+}
+
