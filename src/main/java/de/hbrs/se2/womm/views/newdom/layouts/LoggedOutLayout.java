93809eabdd0dcbe9a9db26d514f6796a2da4685d
Thiemo Ottersbach <thiemo.ott@gmail.com>
2023-11-08 21:24:57 +0100

    feat(vaadin): Adding Student Register View

--- src/main/java/de/hbrs/se2/womm/views/newdom/layouts/LoggedOutLayout.java
+++ src/main/java/de/hbrs/se2/womm/views/newdom/layouts/LoggedOutLayout.java
@@ -6,6 +6,7 @@ import de.hbrs.se2.womm.views.UnternehmenPreviewView;
 import de.hbrs.se2.womm.views.newdom.HomepageUnternehmenView;
 import de.hbrs.se2.womm.views.newdom.LoginViewDo;
 import de.hbrs.se2.womm.views.newdom.PreviewDo;
+import de.hbrs.se2.womm.views.StudentRegisterView;
 //import de.hbrs.se2.womm.views.newdom.PreviewDo;
 
 public class LoggedOutLayout extends AbstractLayout {
@@ -19,7 +20,7 @@ public class LoggedOutLayout extends AbstractLayout {
     void createDrawer() {
         addToDrawer(new VerticalLayout(
                 new RouterLink("Login", LoginViewDo.class),
-                new RouterLink("Register - ToDo", LoginViewDo.class),
+                new RouterLink("Register - ToDo", StudentRegisterView.class),
                 new RouterLink("Preview/LandingPage? ", PreviewDo.class),
                 new RouterLink("UnternehmenPreviewView", UnternehmenPreviewView.class)
         ));
