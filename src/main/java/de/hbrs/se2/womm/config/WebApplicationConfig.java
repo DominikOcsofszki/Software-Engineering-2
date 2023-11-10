ea08ecb697a428209d73c88aaf66b410ac235a10
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-10 10:12:08 +0100

    fix: adding redirects

---
+++ src/main/java/de/hbrs/se2/womm/config/WebApplicationConfig.java
@@ -0,0 +1,30 @@
+//package de.hbrs.se2.womm.config;
+//
+////public class WebApplicationConfig {
+////}
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebApplicationConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+////        registry.addViewController("").setViewName("forward:/vaadin/Preview");
+//        registry.addViewController("/notFound").setViewName("forward:/index.html");
+//    }
+//
+//    @Override
+//
+//
+////    @Bean
+////    public EmbeddedServletContainerCustomizer containerCustomizer() {
+////        return container -> {
+////            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+////                    "/notFound"));
+////        };
+////    }
+//
+//}
