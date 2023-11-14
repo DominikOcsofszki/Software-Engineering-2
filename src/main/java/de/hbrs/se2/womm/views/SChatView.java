d4a3f051be781fb1f3074731a06605b732a550ed
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-14 16:05:21 +0100

    feat(SChatView): Dividing views that both use into two each. Adding S, U. change Layout

--- src/main/java/de/hbrs/se2/womm/views/SChatView.java
+++ src/main/java/de/hbrs/se2/womm/views/SChatView.java
@@ -4,10 +4,11 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hbrs.se2.womm.views.newdom.layouts.BothLayout;
+import de.hbrs.se2.womm.views.newdom.layouts.StudentLayout;
 import jakarta.annotation.security.RolesAllowed;
 
-@Route(value = "ChatView", layout = BothLayout.class)
+@Route(value = "ChatView", layout = StudentLayout.class)
 @PageTitle("ChatView")
-@RolesAllowed({"UNTERNEHMEN", "ADMIN", "STUDENT"})
-public class ChatView extends VerticalLayout {
+@RolesAllowed({ "ADMIN", "STUDENT"})
+public class SChatView extends VerticalLayout {
 }
