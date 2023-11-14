301d9916412d8c470f6c5367ee44eef880ad86cb
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-14 16:05:37 +0100

    feat(SFirmProfileDisplayView): Dividing views that both use into two each. Adding S, U. change Layout

--- src/main/java/de/hbrs/se2/womm/views/SFirmProfileDisplayView.java
+++ src/main/java/de/hbrs/se2/womm/views/SFirmProfileDisplayView.java
@@ -4,11 +4,12 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hbrs.se2.womm.views.newdom.layouts.BothLayout;
+import de.hbrs.se2.womm.views.newdom.layouts.StudentLayout;
 import jakarta.annotation.security.RolesAllowed;
 
-@Route(value = "FirmProfileDisplayView", layout = BothLayout.class)
+@Route(value = "FirmProfileDisplayView", layout = StudentLayout.class)
 @PageTitle("FirmProfileDisplayView")
-@RolesAllowed({"UNTERNEHMEN", "ADMIN", "STUDENT"})
-public class FirmProfileDisplayView extends VerticalLayout {
+@RolesAllowed({"ADMIN", "STUDENT"})
+public class SFirmProfileDisplayView extends VerticalLayout {
 
 }
