b551e60dfdb1931be2a768b2ad0b375f02073728
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-15 16:45:07 +0100

    feat(ROUTING): Adding Routing with all Views

--- src/main/java/de/hbrs/se2/womm/views/layouts/ROUTING.java
+++ src/main/java/de/hbrs/se2/womm/views/layouts/ROUTING.java
@@ -1,13 +1,35 @@
 package de.hbrs.se2.womm.views.layouts;
 
 public class ROUTING {
+    static public class ALL {
+        public static final String LandingPageView = "LandingPageView";
+        public static final String LoginView = "LoginView";
+        public static final String RegistrierungStudentView = "RegistrierungStudentView";
+    }
+
     static public class STUDENT {
-        public static final String APPLICATIONS = "SApplicationView";
-        public static final String CHAT = "SChatView";
+
+        public static final String SAboStudentView = "SAboStudentView";
+        public static final String SApplicationView = "SApplicationView";
+        public static final String SChatView = "SChatView";
+        public static final String SCreateChangeStudentProfileView = "SCreateChangeStudentProfileView";
+        public static final String SFirmProfileDisplayView = "SFirmProfileDisplayView";
+        public static final String SHomepageStudentView = "SHomepageStudentView";
+        public static final String SJobProjectWorkshopDisplayView = "SJobProjectWorkshopDisplayView";
+        public static final String SNotificationView = "SNotificationView";
+        public static final String StudentProfileDisplayView = "StudentProfileDisplayView";
+
+
     }
     public class UNTERNEHMEN {
-        public static final String APPLICATIONS = "UApplicationsView";
-        public static final String CHAT = "UChatView";
-        public static final String EditFirmProfileDisplay = "EditFirmProfileDisplayView";
+        public static final String UApplicationView = "UApplicationView";
+        public static final String UApplicationsView = "UApplicationsView";
+        public static final String UChatView = "UChatView";
+        public static final String UEditFirmProfileDisplayView = "UEditFirmProfileDisplayView";
+        public static final String UFirmProfileDisplayView = "UFirmProfileDisplayView";
+        public static final String UHomepageUnternehmenView = "UHomepageUnternehmenView";
+        public static final String UStelleAnzeigeErstellenView = "UStelleAnzeigeErstellenView";
+        public static final String UUnternehmenPreviewView = "UUnternehmenPreviewView";
     }
 }
+
