c25a1f6cfa38cbf7a5935cddf1c12f625749b9fa
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-15 15:59:34 +0100

    feat: Adding ROUTING

---
+++ src/main/java/de/hbrs/se2/womm/views/ROUTING.java
@@ -0,0 +1,13 @@
+package de.hbrs.se2.womm.views;
+
+public class ROUTING {
+    static public class STUDENT {
+        public static final String APPLICATIONS = "SApplicationView";
+        public static final String CHAT = "SChatView";
+    }
+    public class UNTERNEHMEN {
+        public static final String APPLICATIONS = "UApplicationsView";
+        public static final String CHAT = "UChatView";
+        public static final String EditFirmProfileDisplay = "EditFirmProfileDisplayView";
+    }
+}
