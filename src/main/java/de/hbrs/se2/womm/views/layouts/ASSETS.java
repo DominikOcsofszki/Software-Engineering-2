efaaabd0698747413a346e1277f75eec7c7776d0
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-15 18:09:49 +0100

    feat(ASSETS): Adding ASSETS for files/imgs

---
+++ src/main/java/de/hbrs/se2/womm/views/layouts/ASSETS.java
@@ -0,0 +1,31 @@
+package de.hbrs.se2.womm.views.layouts;
+
+public class ASSETS {
+
+    static public class LOGO {
+
+    }
+        static public class IMG {
+
+        public static String IMG1=  "themes/theme_1/Hiring_pic.jpg";
+        public static String IMG2=  "themes/theme_1/Womm_big_logo.png";
+        public static String IMG3=  "themes/theme_1/Womm_text_logo.png";
+        public static String IMG4=  "themes/theme_1/banner.jpg";
+        public static String IMG5=  "themes/theme_1/banner_new.jpg";
+        public static String IMG6=  "themes/theme_1/logo.png";
+        public static String IMG7=  "themes/theme_1/previewPage.png";
+        public static String IMG8=  "themes/theme_1/user.png";
+
+    }
+
+    static public class ICONS {
+    }
+
+    static public class BANNER {
+
+    }
+    static public class RANDOM {
+       public static String USER=  "themes/theme_1/user.png";
+
+    }
+}
