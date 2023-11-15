7d17cd9f88870064f87d872b8d9298ac9fd78bda
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-15 11:10:25 +0100

    feat(redirect): add RedirectController to vaadin pages

--- src/main/java/de/hbrs/se2/womm/config/RedirectController.java
+++ src/main/java/de/hbrs/se2/womm/config/RedirectController.java
@@ -7,14 +7,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("tmpDelete")
+@RequestMapping("")
 public class RedirectController {
 
     @GetMapping("")
     public RedirectView redirectWithUsingRedirectView(RedirectAttributes attributes) {
-        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
-        //ToDo adding Security for redirecting for each different user
-        return new RedirectView("vaadin/login");
+        return new RedirectView("vaadin/");
     }
 
 }
