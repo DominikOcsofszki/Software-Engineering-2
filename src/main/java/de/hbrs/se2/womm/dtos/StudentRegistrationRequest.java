064119e5f9c496a0efe1da2b0b4d4a3099000173
agrgic2s <antonio.grgic@smail.inf.h-brs.de>
2023-11-15 12:47:10 +0100

    feat(dto): enabled String to Date mapping

--- src/main/java/de/hbrs/se2/womm/dtos/StudentRegistrationRequest.java
+++ src/main/java/de/hbrs/se2/womm/dtos/StudentRegistrationRequest.java
@@ -1,5 +1,6 @@
 package de.hbrs.se2.womm.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -9,5 +10,6 @@ import java.util.Date;
 public class StudentRegistrationRequest extends RegistrationRequest {
     private String firstname;
     private String lastname;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date dob;
 }
--- src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
+++ src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
@@ -1,5 +1,6 @@
 package de.hbrs.se2.womm.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,6 +11,7 @@ public class UnternehmenDTO extends NutzerDTO {
     private Long unternehmenId;
     private String name;
     private String beschreibung;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date gruendung;
     private Long nutzerId;
 }
