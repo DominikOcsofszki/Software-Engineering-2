7a267c4ccbbca8097fa1fa3c1aebea9176c31c71
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-16 10:18:31 +0100

    fix(CONFIG.DB.USING): add CONFIG

--- src/main/java/de/hbrs/se2/womm/config/CONFIG.java
+++ src/main/java/de/hbrs/se2/womm/config/CONFIG.java
@@ -2,8 +2,8 @@ package de.hbrs.se2.womm.config;
 
 public class CONFIG {
     static public class DB {
-        public static final String TEST=  "test";
-        public static final String SE=  "se";
+        public static final String TEST =  "test";
+        public static final String SE =  "se";
         public static final String USING =  SE;
 
     }
