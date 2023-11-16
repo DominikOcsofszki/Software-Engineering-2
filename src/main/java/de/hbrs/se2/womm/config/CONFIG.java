668723decf2d3eacd46027849012f75101a69085
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-16 09:51:53 +0100

    feat(CONFIG.java): add configs

---
+++ src/main/java/de/hbrs/se2/womm/config/CONFIG.java
@@ -0,0 +1,10 @@
+package de.hbrs.se2.womm.config;
+
+public class CONFIG {
+    static public class DB {
+        public static final String TEST=  "test";
+        public static final String SE=  "se";
+        public static final String USING =  SE;
+
+    }
+}
