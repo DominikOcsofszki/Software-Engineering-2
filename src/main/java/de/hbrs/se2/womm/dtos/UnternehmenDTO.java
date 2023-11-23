cd2dcccc57d26d752571ac78081604edc85dd817
Jan Kölzer <jan.koelzer@smail.inf.h-brs.de>
2023-11-23 16:58:19 +0100

    feat(UnternehmenDTO): Getter & Builder

--- src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
+++ src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
@@ -1,10 +1,14 @@
 package de.hbrs.se2.womm.dtos;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.Date;
 
 @Data
+@Builder
+@Getter
 public class UnternehmenDTO {
     private Long unternehmenId;
     private String name;
