86adb69ea04c380d64a0750eb77d5a063b6d3dbe
Jan Kölzer <jan.koelzer@smail.inf.h-brs.de>
2023-11-23 16:46:48 +0100

    fix(Unternehmen): Umstellung des Attributes gruendung auf String anstelle von Date. To-do: Frontendseitig sichern, dass das date im Format YYYY-MM-DD

--- src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
+++ src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
@@ -9,6 +9,6 @@ public class UnternehmenDTO {
     private Long unternehmenId;
     private String name;
     private String beschreibung;
-    private Date gruendung;
+    private String gruendung;
     private Long nutzerId;
 }
