82fac8732ee2d88d9433d80b1e8bdbac891a6bd6
Jan Kölzer <jan.koelzer@smail.inf.h-brs.de>
2023-11-23 19:19:58 +0100

    fix(CompanyController): CompanyController.java heißt jetzt UnternehmenController

---
+++ src/main/java/de/hbrs/se2/womm/controller/UnternehmenController.java
@@ -0,0 +1,51 @@
+package de.hbrs.se2.womm.controller;
+
+import de.hbrs.se2.womm.dtos.UnternehmenDTO;
+import de.hbrs.se2.womm.services.UnternehmenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UnternehmenController {
+    UnternehmenService unternehmenService;
+
+    public UnternehmenController(UnternehmenService unternehmenService){
+        this.unternehmenService = unternehmenService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Void> getAllUnternehmen() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/Unternehmen/{id}")
+    public ResponseEntity<UnternehmenDTO> getUnternehmenById(@PathVariable long id){
+        UnternehmenDTO gefunden = unternehmenService.getUnternehmenPerID(id);
+        return new ResponseEntity<>(gefunden,HttpStatus.OK);
+    }
+
+    @GetMapping("/Unternehmen")
+    public ResponseEntity<List<UnternehmenDTO>> getUnternehmenByName(@RequestParam(name = "name") String name) {
+         List<UnternehmenDTO> gefunden = unternehmenService.getUnternehmenDTOPerName(name);
+        return new ResponseEntity<>(gefunden,HttpStatus.OK);
+    }
+
+    @PatchMapping("/Unternehmen/{id}")
+    public ResponseEntity<Void> updateUnternehmen(@PathVariable String id, @RequestBody UnternehmenDTO request) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/Unternehmen/{id}/offers")
+    public ResponseEntity<Void> getAllOffersByUnternehmen(@PathVariable String id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/Unternehmen/{UnternehmenId}/offer/{offerId}")
+    public ResponseEntity<Void> getAllOffersByUnternehmen(@PathVariable String UnternehmenId, @PathVariable String offerId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
