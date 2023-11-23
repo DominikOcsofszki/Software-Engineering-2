328d5cb6973400104b4441a5ab6ca7e50c4fc0e3
Jan Koelzer <jan.koelzer@smail.inf.h-brs.de>
2023-11-23 12:42:23 +0100

    Revert "Merge branch 'user-registration' into 'main'"
    This reverts merge request !39

--- src/main/java/de/hbrs/se2/womm/controller/AuthenticationController.java
+++ src/main/java/de/hbrs/se2/womm/controller/AuthenticationController.java
@@ -1,41 +1,34 @@
 package de.hbrs.se2.womm.controller;
 
-import de.hbrs.se2.womm.dtos.*;
-import de.hbrs.se2.womm.exceptions.UsernameAlreadyTakenException;
-import de.hbrs.se2.womm.services.AuthenticationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import de.hbrs.se2.womm.dtos.LoginRequest;
+import de.hbrs.se2.womm.dtos.LogoutRequest;
+import de.hbrs.se2.womm.dtos.RegistrationRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
-    @Autowired
-    private AuthenticationService authenticationService;
+    // TODO add logic to methods and suitable return types for ResponseEntities
 
-    @PostMapping("/register/student")
-    public ResponseEntity<Void> registerStudent(@RequestBody StudentRegistrationRequest request) throws UsernameAlreadyTakenException {
-        authenticationService.registerStudent(request);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @PostMapping("/register/company")
-    public ResponseEntity<Void> registerCompany(@RequestBody CompanyRegistrationRequest request) throws UsernameAlreadyTakenException {
-        authenticationService.registerCompany(request);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping("/register")
+    public ResponseEntity<Void> registerUser(@RequestBody RegistrationRequest request) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/login")
     public ResponseEntity<Void> loginUser(@RequestBody LoginRequest request) {
-        // TODO: add logic
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logoutUser(@RequestBody LogoutRequest request) {
-        // TODO: add logic
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
--- src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
+++ src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
@@ -1,17 +1,14 @@
 package de.hbrs.se2.womm.dtos;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.util.Date;
+
 @Data
-@EqualsAndHashCode(callSuper = true)
-public class UnternehmenDTO extends NutzerDTO {
+public class UnternehmenDTO {
     private Long unternehmenId;
     private String name;
     private String beschreibung;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date gruendung;
     private Long nutzerId;
 }
