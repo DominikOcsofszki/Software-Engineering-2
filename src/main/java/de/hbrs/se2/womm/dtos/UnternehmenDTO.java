c51f18bb6be03428ccda33a9926225d70184c829
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-25 21:16:49 +0100

    feat(UnternehmenDTO.java): Adding getImage

--- src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
+++ src/main/java/de/hbrs/se2/womm/dtos/UnternehmenDTO.java
@@ -1,11 +1,13 @@
 package de.hbrs.se2.womm.dtos;
 
 import de.hbrs.se2.womm.entities.Nutzer;
+import de.hbrs.se2.womm.views.layouts.ASSETS;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
-import java.util.Date;
+//import java.awt.*;
+import com.vaadin.flow.component.html.Image;
 
 @Data
 @Builder
@@ -16,4 +18,55 @@ public class UnternehmenDTO {
     private String beschreibung;
     private String gruendung;
     private Nutzer nutzer;
+    private Image getPlaceholderImage(){
+        return ASSETS.buildPlaceholder(50,50);
+//        return new com.vaadin.flow.component.html.Image("data:image/png;base64,"
+//                + nutzer.getNutzerProfilbild(), "Profilbild");
+    }
+    public Image getImage(){
+        return getPlaceholderImage();
+    }
+
+
+//    public static boolean filterFunction(UnternehmenDTO anyDTO, String inputSearchNameFilter, SEARCHFILTER searchBy) {
+//
+//        inputSearchNameFilter = inputSearchNameFilter.toLowerCase();
+//        String checkUnternehmen = switch (searchBy) {
+////            case unternehmenId -> itemsToStringToLowerCase(anyDTO.getUnternehmenId()).toString().toLowerCase();
+//            case UNTERNEHMENID -> itemsToStringToLowerCase(anyDTO.getUnternehmenId());
+//            case NAME -> itemsToStringToLowerCase(anyDTO.getName());
+//            case BESCHREIBUNG -> itemsToStringToLowerCase(anyDTO.getBeschreibung());
+//            case GRUENDUNG -> itemsToStringToLowerCase(anyDTO.getGruendung());
+//            case NUTZER -> itemsToStringToLowerCase(anyDTO.getNutzer().toString());
+//            default -> throw new IllegalStateException("Unexpected value: " + searchBy);
+//        };
+////        String checkUnternehmen;
+////        switch (searchBy) {
+////            case unternehmenId:
+////                checkUnternehmen = anyDTO.getUnternehmenId().toString().toLowerCase();
+////                break;
+////            case name:
+//////                checkUnternehmen = anyDTO.getName().toLowerCase();
+//////                break;
+//////            case beschreibung:
+//////                checkUnternehmen = anyDTO.getBeschreibung().toLowerCase();
+//////                break;
+//////            case gruendung:
+//////                checkUnternehmen = anyDTO.getGruendung().toLowerCase();
+//////                break;
+//////            case nutzer:
+//////                checkUnternehmen = anyDTO.getNutzer().toString().toLowerCase();
+//////                break;
+////            default:
+////                throw new IllegalStateException("Unexpected value: " + searchBy);
+////        }
+//
+//        return checkUnternehmen.contains(inputSearchNameFilter);
+//    }
+//    private static String itemsToStringToLowerCase(Object item) {
+//        return item.toString().toLowerCase();
+//    }
+
+
+
 }
