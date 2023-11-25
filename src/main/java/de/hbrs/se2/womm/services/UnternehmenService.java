511c88416c75d5d41cb1e92c32b385b99dd8b2c9
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-25 21:21:13 +0100

    feat(UnternehmenService): Adding FilterMethods as interface to getAll()

--- src/main/java/de/hbrs/se2/womm/services/UnternehmenService.java
+++ src/main/java/de/hbrs/se2/womm/services/UnternehmenService.java
@@ -3,16 +3,14 @@ package de.hbrs.se2.womm.services;
 import de.hbrs.se2.womm.dtos.UnternehmenDTO;
 import de.hbrs.se2.womm.mapper.UnternehmenMapper;
 import de.hbrs.se2.womm.repositories.UnternehmenRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import javax.inject.Inject;
 import java.util.List;
 
 @Service
-public class UnternehmenService {
+public class UnternehmenService implements FilterMethods{
     private final UnternehmenRepository unternehmenRepository;
-    private final UnternehmenMapper mapper = UnternehmenMapper.INSTANCE;
+    private final UnternehmenMapper unternehmenMapper = UnternehmenMapper.INSTANCE;
 
     public UnternehmenService(UnternehmenRepository unternehmenRepository) {
         this.unternehmenRepository = unternehmenRepository;
@@ -20,18 +18,24 @@ public class UnternehmenService {
 
     public UnternehmenDTO getUnternehmenPerID(long id){
         return unternehmenRepository.findUnternehmenByUnternehmenId(id)
-                .map(mapper::unternehmenZuDTO)
+                .map(unternehmenMapper::unternehmenZuDTO)
                 .orElse(null);
     }
 
     public List<UnternehmenDTO> getUnternehmenDTOPerName(String unternehmenName) {
         return unternehmenRepository.findUnternehmenByUnternehmenNameIgnoreCaseContaining(unternehmenName)
                 .stream()
-                .map(mapper::unternehmenZuDTO).toList();
+                .map(unternehmenMapper::unternehmenZuDTO).toList();
     }
-    public List<UnternehmenDTO> findAllUnternehmen() {
-        return unternehmenRepository.findAll()
+    public List<UnternehmenDTO> getAllUnternehmen() {
+        return unternehmenRepository
+                .findAll()
                 .stream()
-                .map(mapper::unternehmenZuDTO).toList();
+                .map(unternehmenMapper::unternehmenZuDTO)
+                .toList();
+    }
+    @Override
+    public List<UnternehmenDTO> getAll() {
+        return getAllUnternehmen();
     }
 }
