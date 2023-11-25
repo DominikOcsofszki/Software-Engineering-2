2e4839d0fcc0710f7fff3f1d63253183925ad4fd
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-25 23:02:33 +0100

    feat(StudentDTO.java): Adding getAllFilter and getImage

--- src/main/java/de/hbrs/se2/womm/dtos/StudentDTO.java
+++ src/main/java/de/hbrs/se2/womm/dtos/StudentDTO.java
@@ -1,6 +1,8 @@
 package de.hbrs.se2.womm.dtos;
 
+import com.vaadin.flow.component.html.Image;
 import de.hbrs.se2.womm.entities.Nutzer;
+import de.hbrs.se2.womm.views.layouts.ASSETS;
 import lombok.Data;
 
 @Data
@@ -14,4 +16,26 @@ public class StudentDTO {
     private String studentBio;
     private String studentSpezialisierung;
     private Integer studentSemester;
+
+    private com.vaadin.flow.component.html.Image getPlaceholderImage(){
+        return ASSETS.buildPlaceholder(50,50);
+    }
+    public Image getImage(){
+        if(nutzer == null || nutzer.getNutzerProfilbild() == null) return getPlaceholderImage();
+        return new com.vaadin.flow.component.html.Image("data:image/png;base64,"
+                + nutzer.getNutzerProfilbild(), "getImage");
+    }
+    public static String[] getAllFilter(){
+        return new String[]{
+                "nutzer",
+                "studentId",
+                "studentVorname",
+                "studentName",
+                "studentGeburtstag",
+                "studentBenachrichtigung",
+                "studentBio",
+                "studentSpezialisierung",
+                "studentSemester"
+        };
+    }
 }
