6d856c8ed440060d1aa8eb5819905c3cda0b6c40
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-26 21:43:38 +0100

    feat(FilterView): update FilterView

--- src/main/java/de/hbrs/se2/womm/views/components/FilterView.java
+++ src/main/java/de/hbrs/se2/womm/views/components/FilterView.java
@@ -6,9 +6,11 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import de.hbrs.se2.womm.controller.StelleController;
 import de.hbrs.se2.womm.controller.StudentController;
 import de.hbrs.se2.womm.services.ImageService;
 import de.hbrs.se2.womm.services.StudentService;
+import de.hbrs.se2.womm.views.components.copyOnly.ComponentFilterGridControllerStellen;
 import de.hbrs.se2.womm.views.layouts.LoggedOutLayout;
 
 @PageTitle("MainView")
@@ -16,18 +18,22 @@ import de.hbrs.se2.womm.views.layouts.LoggedOutLayout;
 @Route(value = "filter", layout = LoggedOutLayout.class)
 public class FilterView extends VerticalLayout {
 
-    public FilterView(StudentController studentController){
-//        add(new ComponentMusterFilterGridControllerStudent(studentController));
-    add(new ComponentMusterFilterGridController(studentController));
-
-//        add(new ComponentMusterFilterGridController(studentController));
-//        add(new ComponentMusterFilterGridControllerStudent(studentController));
-
-//        add(new ComponentUnternehmenFilterGridAPI());
-
-//        add(new ComponentStudentsFilterGrid(studentService));
-        ImageService imageService = new ImageService();
-        Image newImge = imageService.test();
-        add(newImge);
+    public FilterView(StelleController controller){
+        add(new ComponentFilterGridControllerStellen(controller));
     }
+
+//    public FilterView(StudentController studentController){
+////        add(new ComponentMusterFilterGridControllerStudent(studentController));
+//    add(new ComponentMusterFilterGridController(studentController));
+//
+////        add(new ComponentMusterFilterGridController(studentController));
+////        add(new ComponentMusterFilterGridControllerStudent(studentController));
+//
+////        add(new ComponentUnternehmenFilterGridAPI());
+//
+////        add(new ComponentStudentsFilterGrid(studentService));
+//        ImageService imageService = new ImageService();
+//        Image newImge = imageService.test();
+//        add(newImge);
+//    }
 }
