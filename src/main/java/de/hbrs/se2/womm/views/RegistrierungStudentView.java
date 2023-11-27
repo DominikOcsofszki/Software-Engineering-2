d71b944484473ff448a1a153063bbaecdc354d63
Thiemo <thiemo.ott@gmail.com>
2023-11-27 22:15:51 +0100

    feat(vaadin): Added username, dob and location field to RegistrierungStudentView

--- src/main/java/de/hbrs/se2/womm/views/RegistrierungStudentView.java
+++ src/main/java/de/hbrs/se2/womm/views/RegistrierungStudentView.java
@@ -2,17 +2,25 @@ package de.hbrs.se2.womm.views;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import de.hbrs.se2.womm.controller.AuthenticationController;
+import de.hbrs.se2.womm.dtos.StudentRegistrationRequest;
 import de.hbrs.se2.womm.views.layouts.LoggedOutLayout;
 import de.hbrs.se2.womm.views.layouts.ROUTING;
+import org.springframework.http.HttpStatusCode;
+
+import java.time.LocalDate;
+import java.util.Locale;
 
 @Route(value = ROUTING.ALL.RegistrierungStudentView, layout = LoggedOutLayout.class)
 @AnonymousAllowed
@@ -21,55 +29,118 @@ public class RegistrierungStudentView extends VerticalLayout {
 
     public RegistrierungStudentView() {
         super();
-
-        add(new HorizontalLayout());
-
         setSizeFull();
 
         setAlignItems(Alignment.CENTER);
 
         setJustifyContentMode(JustifyContentMode.CENTER);
 
-        H4 title = new H4("Student Registration");
+        add(new H4("Student/in Registration"));
+
+        var nameComponent = new com.vaadin.flow.component.textfield.TextField("Name");
+        nameComponent.setTooltipText("Your REAL Name");
+        nameComponent.setRequired(true);
+        nameComponent.setRequiredIndicatorVisible(true);
+        nameComponent.setErrorMessage("Name is required");
+
+        var surnameComponent = new com.vaadin.flow.component.textfield.TextField("Surname");
+        surnameComponent.setTooltipText("Your REAL surname");
+        surnameComponent.setClearButtonVisible(true);
+        surnameComponent.setRequired(true);
+        surnameComponent.setRequiredIndicatorVisible(true);
+        surnameComponent.setErrorMessage("Surname is required");
+
+        var usernameComponent = new com.vaadin.flow.component.textfield.TextField("Username");
+        usernameComponent.setTooltipText("Your desired username");
+        usernameComponent.setRequired(true);
+        usernameComponent.setRequiredIndicatorVisible(true);
+        usernameComponent.setErrorMessage("Username is required");
+
+        EmailField emailComponent = new EmailField("Email \uD83D\uDE33");
+        emailComponent.setTooltipText("Email connected to your future account");
+        emailComponent.setRequired(true);
+        emailComponent.setRequiredIndicatorVisible(true);
+        emailComponent.setPrefixComponent(VaadinIcon.ENVELOPE.create());
+        emailComponent.setErrorMessage("Email is required");
+
+        PasswordField passwordComponent = new PasswordField("Password");
+        passwordComponent.setTooltipText("The password used for login");
+        passwordComponent.setRequired(true);
+        passwordComponent.setRequiredIndicatorVisible(true);
+        passwordComponent.setErrorMessage("Password is required");
+
+        PasswordField passwordConfirmComponent = new PasswordField("Confirm Password");
+        passwordConfirmComponent.setTooltipText("Repeat your password");
+        passwordConfirmComponent.setRequired(true);
+        passwordConfirmComponent.setRequiredIndicatorVisible(true);
+        passwordConfirmComponent.setErrorMessage("Password Confirmation is required");
+
+        DatePicker.DatePickerI18n singleFormatI18n = new DatePicker.DatePickerI18n();
+        singleFormatI18n.setDateFormat("dd.MM.yyyy");
+        DatePicker dateOfBirthComponent = new DatePicker("Date of Birth");
+        dateOfBirthComponent.setI18n(singleFormatI18n);
+        dateOfBirthComponent.setTooltipText("Select your date of birth");
+        dateOfBirthComponent.setRequired(true);
+        dateOfBirthComponent.setRequiredIndicatorVisible(true);
+        dateOfBirthComponent.setErrorMessage("Invalid date given. Dates must follow the 'DD.MM.YYYY' format.");
+
+        var locationComponent = new com.vaadin.flow.component.textfield.TextField("Location");
+        locationComponent.setTooltipText("Your current living location");
+        locationComponent.setRequired(true);
+        locationComponent.setRequiredIndicatorVisible(true);
+        locationComponent.setPrefixComponent(VaadinIcon.PIN.create());
+        locationComponent.setErrorMessage("Location is required");
 
 
-        var name = new com.vaadin.flow.component.textfield.TextField("Name");
-        name.setTooltipText("Your REAL Name");
-        name.setRequired(true);
-        name.setRequiredIndicatorVisible(true);
-        name.setErrorMessage("Name is required");
+        Button registerComponent = new Button("Register", event -> {
+            /*
+            TODO
+            try {
+                StudentRegistrationRequest request = new StudentRegistrationRequest();
+                request.setFirstname(nameComponent.getValue());
+                request.setLastname(surnameComponent.getValue());
+                request.setEmail(emailComponent.getValue());
+                request.setUsername(usernameComponent.getValue());
+                request.setPassword(passwordComponent.getValue());
 
-        var surname = new com.vaadin.flow.component.textfield.TextField("Surname");
-        surname.setTooltipText("Your REAL surname");
-        surname.setClearButtonVisible(true);
-        surname.setRequired(true);
-        surname.setRequiredIndicatorVisible(true);
-        surname.setErrorMessage("Surname is required");
+                //Outputs this date as a String, such as 2007-12-03.
+                //The output will be in the ISO-8601 format yyyy-MM-dd.
+                request.setDob(dateOfBirthComponent.getValue().toString());
 
-        PasswordField password = new PasswordField("Password");
-        password.setTooltipText("The password used for login");
-        password.setRequired(true);
-        password.setRequiredIndicatorVisible(true);
-        password.setErrorMessage("Password is required");
+                //es gibt kein setLocation????
+                request.setLocation(locationComponent.getValue());
 
-        PasswordField passwordConfirm = new PasswordField("Confirm Password");
-        passwordConfirm.setTooltipText("Repeat your password");
-        passwordConfirm.setRequired(true);
-        passwordConfirm.setRequiredIndicatorVisible(true);
-        passwordConfirm.setErrorMessage("Password Confirmation is required");
+                HttpStatusCode answer = (new AuthenticationController()).registerUser(request).getStatusCode();
 
-        EmailField email = new EmailField("Email \uD83D\uDE33");
-        email.setTooltipText("Email connected to your future account");
-        email.setRequired(true);
-        email.setRequiredIndicatorVisible(true);
-        email.setPrefixComponent(VaadinIcon.ENVELOPE.create());
-        email.setErrorMessage("Email is required");
+                if(answer.is2xxSuccessful()){
+                    UI.getCurrent().navigate(ROUTING.STUDENT.SHomepageStudentView);
+                    add(new H2("Registrirung Erfolgreich"));
+                } else {
 
-        Button reg = new Button("Register", event -> {
-            //TODO
-            UI.getCurrent().getPage().reload();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            */
         });
 
-        add(title, name, surname, password, passwordConfirm, email, reg);
+        FormLayout formLayout = new FormLayout();
+
+        formLayout.add(
+                nameComponent,
+                surnameComponent,
+                usernameComponent,
+                emailComponent,
+                passwordComponent,
+                passwordConfirmComponent,
+                dateOfBirthComponent,
+                locationComponent
+        );
+
+        add(
+                formLayout,
+                registerComponent
+        );
     }
 }
