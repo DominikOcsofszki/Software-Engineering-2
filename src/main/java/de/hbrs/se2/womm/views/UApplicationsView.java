0b442829b4fb4615a003da91cdb501de36be2e6d
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-28 16:45:39 +0100

    feat(Generator into Views): UApplicationsView.java

--- src/main/java/de/hbrs/se2/womm/views/UApplicationsView.java
+++ src/main/java/de/hbrs/se2/womm/views/UApplicationsView.java
@@ -3,6 +3,7 @@ package de.hbrs.se2.womm.views;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import de.hbrs.se2.womm.config.SecurityService;
 import de.hbrs.se2.womm.controller.ApplicationController;
 import de.hbrs.se2.womm.controller.BewerbungController;
 import de.hbrs.se2.womm.views.components.using.ComponentFilterGridControllerBewerbung;
@@ -14,8 +15,9 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed({"UNTERNEHMEN","ADMIN"})
 @PageTitle("ApplicationsView")
 public class UApplicationsView extends VerticalLayout {
-
-    public UApplicationsView(BewerbungController controller){
-        add(new ComponentFilterGridControllerBewerbung(controller));
+    private long id;
+    public UApplicationsView(BewerbungController controller, SecurityService securityService) {
+        this.id = securityService.getLoggedInId();
+        add(new ComponentFilterGridControllerBewerbung(controller,this.id));
     }
 }
