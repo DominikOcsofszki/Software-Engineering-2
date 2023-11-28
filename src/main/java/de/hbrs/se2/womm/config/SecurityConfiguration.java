d1ebefa489824e1b562ebcb340151d22ba0913b9
agrgic2s <antonio.grgic@smail.inf.h-brs.de>
2023-11-28 18:38:16 +0100

    feat(SecurityConfig): enabled global SecurityContext

--- src/main/java/de/hbrs/se2/womm/config/SecurityConfiguration.java
+++ src/main/java/de/hbrs/se2/womm/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -63,5 +64,10 @@ public class SecurityConfiguration extends VaadinWebSecurity {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public void configSecurityContext() {
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_GLOBAL);
     }
 }
