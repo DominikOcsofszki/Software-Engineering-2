05ca4ba0a95f6ab0f8498b43c7cead3b485b5346
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-30 10:00:38 +0100

    fix(IgetAllService): delete

--- src/main/java/de/hbrs/se2/womm/services/IgetAllService.java
+++
@@ -1,9 +0,0 @@
-package de.hbrs.se2.womm.services;
-
-import de.hbrs.se2.womm.dtos.AbstractDTO;
-
-import java.util.List;
-
-public interface IgetAllService {
-    List<? extends AbstractDTO> getAllService();
-}
