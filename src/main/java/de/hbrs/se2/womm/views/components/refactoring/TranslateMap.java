d0eccb84bd3b0b819ef4857df03aa335c4814c40
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-07 10:13:29 +0100

    feat(Translation): adding translation into AbstractViews, Also adding checker that show what it not in the translation yet

--- src/main/java/de/hbrs/se2/womm/views/components/refactoring/TranslateMap.java
+++ src/main/java/de/hbrs/se2/womm/views/components/refactoring/TranslateMap.java
@@ -5,11 +5,12 @@ import java.util.Map;
 
 public class TranslateMap {
     public static Map<String, String> translateMap = new HashMap<>() {{
-        put("buttonToShowMissingTranslated", "buttonToShowMissingTranslated");
-        put("doSthTranslateMe", "doSthTranslateMe<-TranslationNeeded");
-        put("doSth", "doSth<-TranslationNeeded");
-        put("sthNew", "sthNew<-TranslationNeeded");
-        put("Find your dream job on w.o.m.m.", "Finde deinen Traum Beruf auf w.o.m.m.");
+        put("Your job search portal. Discover thousands of job opportunities and more.", "Translated. Discover thousands of job opportunities and more.<-TranslationNeeded");
+        put("SignUp", "SignUp<-TranslationNeeded");
+        put("Find your dream job on w.o.m.m.", "Find your dream job on w.o.m.m.<-TranslationNeeded");
+        put("Want to create company profile? Contact us at kontakt@womm.de.", "Want to create company profile? Contact us at kontakt@womm.de.<-TranslationNeeded");
+        put("LogIn", "LogIn<-TranslationNeeded");
+        put("buttonToShowMissingTranslated", "knopfbla");
 
     }};
 
