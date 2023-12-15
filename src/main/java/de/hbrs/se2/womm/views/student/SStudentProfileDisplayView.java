17b3732961584c163ea92f021988204aa0249063
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-15 13:49:01 +0100

    ref(SStudentProfileDisplayView.java): ref for Service

--- src/main/java/de/hbrs/se2/womm/views/student/SStudentProfileDisplayView.java
+++ src/main/java/de/hbrs/se2/womm/views/student/SStudentProfileDisplayView.java
@@ -11,10 +11,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hbrs.se2.womm.config.SecurityService;
-import de.hbrs.se2.womm.controller.StudentController;
 import de.hbrs.se2.womm.dtos.StudentDTO;
 import de.hbrs.se2.womm.services.StudentService;
-import de.hbrs.se2.womm.views.layouts.AbstractViewDTObyNutzerID;
+import de.hbrs.se2.womm.views.layouts.AViewWomm;
 import de.hbrs.se2.womm.views.layouts.ROUTING;
 import de.hbrs.se2.womm.views.layouts.StudentLayout;
 import jakarta.annotation.security.RolesAllowed;
@@ -22,7 +21,7 @@ import jakarta.annotation.security.RolesAllowed;
 @Route(value = ROUTING.STUDENT.SStudentProfileDisplayView, layout = StudentLayout.class)
 @RolesAllowed({"STUDENT", "ADMIN"})
 @PageTitle("StudentProfileDisplayView")
-public class SStudentProfileDisplayView extends AbstractViewDTObyNutzerID<StudentController, StudentDTO> {
+public class SStudentProfileDisplayView extends AViewWomm {
     StudentDTO studentDTO;
 
     //SetUp Data
