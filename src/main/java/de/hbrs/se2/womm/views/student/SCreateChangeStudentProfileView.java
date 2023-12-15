d90611e799c25c15d279019df3f458ae208cd477
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-15 13:47:02 +0100

    ref(SCreateChangeStudentProfileView.java): refactor for Service

--- src/main/java/de/hbrs/se2/womm/views/student/SCreateChangeStudentProfileView.java
+++ src/main/java/de/hbrs/se2/womm/views/student/SCreateChangeStudentProfileView.java
@@ -15,11 +15,10 @@ import com.vaadin.flow.component.textfield.*;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hbrs.se2.womm.config.SecurityService;
-import de.hbrs.se2.womm.controller.StudentController;
 import de.hbrs.se2.womm.dtos.StudentDTO;
 import de.hbrs.se2.womm.services.StudentService;
 import de.hbrs.se2.womm.views.LandingPageView;
-import de.hbrs.se2.womm.views.layouts.AbstractViewDTObyNutzerID;
+import de.hbrs.se2.womm.views.layouts.AViewWomm;
 import de.hbrs.se2.womm.views.layouts.ROUTING;
 import de.hbrs.se2.womm.views.layouts.StudentLayout;
 import jakarta.annotation.security.RolesAllowed;
@@ -32,7 +31,7 @@ import java.time.LocalDate;
 @RolesAllowed({"STUDENT","ADMIN"})
 @PageTitle("CreateChangeStudentProfileView")
 //public class SCreateChangeStudentProfileView extends AbstractView {
-public class SCreateChangeStudentProfileView extends AbstractViewDTObyNutzerID<StudentController, StudentDTO> {
+public class SCreateChangeStudentProfileView extends AViewWomm {
     StudentDTO studentDTO;
     private long aktuelleNutzerID;
     public SCreateChangeStudentProfileView(StudentService studentService, SecurityService securityService) {
