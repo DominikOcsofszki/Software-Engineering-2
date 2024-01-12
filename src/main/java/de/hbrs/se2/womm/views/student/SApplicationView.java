bdd30c5a479f3dfca00abef92f46830a4b9e5b4b
Thiemo Ottersbach <thiemo.ott@gmail.com>
2024-01-12 17:38:34 +0100

    feat(vaadin): Add Stelle information to SApplicationView

--- src/main/java/de/hbrs/se2/womm/views/student/SApplicationView.java
+++ src/main/java/de/hbrs/se2/womm/views/student/SApplicationView.java
@@ -1,22 +1,23 @@
 package de.hbrs.se2.womm.views.student;
 
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.Unit;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.*;
 import de.hbrs.se2.womm.dtos.BewerbungDTO;
+import de.hbrs.se2.womm.dtos.StelleDTO;
 import de.hbrs.se2.womm.dtos.StudentDTO;
 import de.hbrs.se2.womm.model.ApplicationStatus;
 import de.hbrs.se2.womm.services.BewerbungService;
+import de.hbrs.se2.womm.services.ImageService;
+import de.hbrs.se2.womm.services.StelleService;
 import de.hbrs.se2.womm.services.StudentService;
-import de.hbrs.se2.womm.views.extra.ASSETS;
+import de.hbrs.se2.womm.views.extra.VaadinBuilderWomm;
 import de.hbrs.se2.womm.views.layouts.AViewWomm;
 import de.hbrs.se2.womm.views.layouts.ROUTING;
 import de.hbrs.se2.womm.views.layouts.StudentLayout;
@@ -31,19 +32,21 @@ import java.util.Optional;
 public class SApplicationView extends AViewWomm implements HasUrlParameter<Long> {
     private final BewerbungService bewerbungService;
     private final StudentService studentService;
+    private final StelleService stelleService;
     private BewerbungDTO bewerbung;
     private String bewerbungText;
     private StudentDTO student;
+    private StelleDTO stelleDTO;
     private Long studentID;
     private String studentName;
     private String studentVorname;
     //private byte[] studentProfilePicture;
 
-    public SApplicationView(BewerbungService bewerbungService,
-                            StudentService studentService) {
+    public SApplicationView(BewerbungService bewerbungService, StudentService studentService, StelleService stelleService) {
         super();
         this.bewerbungService = bewerbungService;
         this.studentService = studentService;
+        this.stelleService = stelleService;
     }
 
 
@@ -51,6 +54,10 @@ public class SApplicationView extends AViewWomm implements HasUrlParameter<Long>
     public void setParameter(BeforeEvent beforeEvent, @OptionalParameter Long bewerbungID) {
         Optional<BewerbungDTO> fetchedBewerbung = bewerbungService.getById(bewerbungID);
         fetchedBewerbung.ifPresent(bewerbungDTO -> this.bewerbung = bewerbungDTO);
+        Optional<StelleDTO> checkStelleDTO = stelleService.getById(bewerbung.getBewerbungStelle().getStelleId());
+        this.stelleDTO = checkStelleDTO.get();
+
+
         setUpApplication();
     }
 
@@ -75,22 +82,12 @@ public class SApplicationView extends AViewWomm implements HasUrlParameter<Long>
     }
 
     private void setUpTop() {
-        HorizontalLayout top = new HorizontalLayout();
-        top.setAlignItems(FlexComponent.Alignment.CENTER);
-        top.setJustifyContentMode(JustifyContentMode.AROUND);
-        Image image = new Image(ASSETS.IMG.IMG8, "placeholder");
-        image.setWidth(200, Unit.PIXELS);
-        image.setHeight(200, Unit.PIXELS);
-        image.getStyle().set("border", "1px solid grey");
-        image.getStyle().set("border-radius", "5px");
-        top.add(image);
-        H3 name = new H3(String.format("%s, %s", studentName, studentVorname));
-        name.getStyle().set("cursor", "pointer");
-        top.add(name);
-        add(top);
+        setUpHeader();
+        setUpStellenanzeige();
     }
 
     private void setUpAnschreiben() {
+        add(new H3(getWommBuilder().translateText("Your motivational letter:")));
         VerticalLayout anschreiben = new VerticalLayout();
         List.of(bewerbungText.split("\n\n")).forEach(paragraph -> {
             List.of(paragraph.split("\n")).forEach(subParagraph -> {
@@ -123,14 +120,84 @@ public class SApplicationView extends AViewWomm implements HasUrlParameter<Long>
             text.getStyle().setColor("red");
             layout.add(text);
         } else if (ApplicationStatus.AUSSTEHEND.toString().equals(bewerbungStatus)) {
-            /*
-            Icon icon = new Icon(VaadinIcon.CHECK_CIRCLE);
-            icon.setColor("green");
+            Icon icon = new Icon(VaadinIcon.HOURGLASS_START);
+            icon.setColor("orange");
             layout.add(icon);
-             */
             text = new H3(getWommBuilder().translateText("Your application is awaiting review."));
+            text.getStyle().setColor("orange");
             layout.add(text);
         }
         add(layout);
+    }
+
+    private void setUpHeader() {
+        HorizontalLayout header = new HorizontalLayout();
+        ImageService imageService = new ImageService();
+        Image i = imageService.getRandomImageHeight(100);
+        header.add(i);
+        String unternehmenName = this.stelleDTO.getUnternehmen().getName();
+        H1 name = new H1(unternehmenName);
+        name.addClickListener(e -> UI.getCurrent()
+                .navigate(ROUTING.STUDENT.SFirmProfileDisplayView + "/" + stelleDTO.getUnternehmen().getUnternehmenId()));
+        header.add(name);
+        add(header);
+    }
+
+
+    private void setUpStellenanzeige() {
+        VerticalLayout stellenanzeige = new VerticalLayout();
+
+        // Ort
+
+        HorizontalLayout ortLayout = new HorizontalLayout();
+        Icon ortsIcon = VaadinIcon.PIN.create();
+        ortLayout.add(ortsIcon);
+
+        Text ort = new Text(this.stelleDTO.getStelleOrt());
+        ortLayout.add(ort);
+
+        stellenanzeige.add(ortLayout);
+
+        // Hyperlink
+
+        HorizontalLayout linkLayout = new HorizontalLayout();
+        Icon linkIcon = VaadinIcon.LINK.create();
+        linkLayout.add(linkIcon);
+
+        Anchor website = new Anchor();
+        String url = this.stelleDTO.getStelleWebsite();
+        website.setText(url);
+
+        linkLayout.add(website);
+
+        stellenanzeige.add(linkLayout);
+
+        // Beschreibung + Header
+
+        Div beschreibung = new Div();
+
+        beschreibung.getStyle().set("margin-top", "20px");
+
+        H3 titel = new H3();
+        String stelleTitel = this.stelleDTO.getStelleTitel();
+        titel.setText(stelleTitel);
+
+        beschreibung.add(titel);
+
+        List<String> paragraphs = List.of(this.stelleDTO.getStelleBeschreibung().split("\n\n"));
+        paragraphs.forEach(paragraph -> {
+            List.of(paragraph.split("\n")).forEach(subParagraph -> {
+                Paragraph newParagraph = new Paragraph();
+                newParagraph.setWidthFull();
+                newParagraph.setText(subParagraph);
+                beschreibung.add(newParagraph);
+            });
+            beschreibung.add(new Html("<br>"));
+        });
+
+        stellenanzeige.add(beschreibung);
+
+        add(stellenanzeige);
+
     }
 }
