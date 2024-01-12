2f308cb2802e0cb95172702e266a0e4095fc6a03
agrgic2s <antonio.grgic@smail.inf.h-brs.de>
2024-01-12 14:16:53 +0100

    refactor(LoginView): deconstructed LoginForm to be able to assign ID to each component;

--- src/main/java/de/hbrs/se2/womm/views/LoginView.java
+++ src/main/java/de/hbrs/se2/womm/views/LoginView.java
@@ -5,13 +5,18 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hbrs.se2.womm.config.SecurityService;
@@ -33,7 +38,6 @@ import org.springframework.security.core.Authentication;
 public class LoginView extends AViewWomm {
 
     SecurityService securityService;
-    LoginForm login = new LoginForm();
 
     LoginView(@Autowired AuthenticationController authenticationController, SecurityService securityService) {
         this.securityService = securityService;
@@ -42,34 +46,49 @@ public class LoginView extends AViewWomm {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
-        login.setId("login-button");
-        login.setForgotPasswordButtonVisible(false);
-        add(
-                new H1("w.o.o.m."),
-                login
-        );
+        VerticalLayout loginFormLayout = new VerticalLayout();
+        loginFormLayout.setAlignItems(Alignment.CENTER);
+        loginFormLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        loginFormLayout.getStyle().setWidth("25%");
+        loginFormLayout.setPadding(true);
+        loginFormLayout.getStyle().setBackground("white");
 
-        login.addLoginListener(loginEvent -> {
+        TextField usernameInput = getWommBuilder().TextField.create(getWommBuilder().translateText("Username"));
+        usernameInput.setWidthFull();
+        usernameInput.setRequired(true);
+        usernameInput.setRequiredIndicatorVisible(true);
+        usernameInput.setErrorMessage(getWommBuilder().translateText("Username is required"));
+
+        PasswordField passwordInput = getWommBuilder().PasswordField.create(getWommBuilder().translateText("Password"));
+        passwordInput.setWidthFull();
+        passwordInput.setRequired(true);
+        passwordInput.setRequiredIndicatorVisible(true);
+        passwordInput.setErrorMessage(getWommBuilder().translateText("Password is required"));
+
+        Button submitButton = getWommBuilder().Button.create(getWommBuilder().translateText("LogIn"));
+        submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        submitButton.addClickListener(e -> {
+            submitButton.setEnabled(false);
             LoginRequest loginRequest = LoginRequest.builder()
-                    .username(loginEvent.getUsername())
-                    .password(loginEvent.getPassword()).build();
+                    .username(usernameInput.getValue())
+                    .password(passwordInput.getValue()).build();
             try {
                 ResponseEntity<Authentication> response = authenticationController.loginUser(loginRequest);
                 if (response.getStatusCode().is2xxSuccessful()) {
                     UI.getCurrent().getPage().setLocation("/");
                 }
-            } catch (AuthenticationException e) {
+            } catch (AuthenticationException error) {
                 Notification notification = new Notification();
                 notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
 
-                Div text = new Div(new Text(e.getMessage()));
+                Div text = new Div(new Text(error.getMessage()));
 
                 Button closeButton = new Button(new Icon("lumo", "cross"));
                 closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
                 closeButton.setAriaLabel("Close");
                 closeButton.addClickListener(event -> {
                     notification.close();
-                    login.setEnabled(true);
+                    submitButton.setEnabled(true);
                 });
 
                 HorizontalLayout layout = new HorizontalLayout(text, closeButton);
@@ -79,6 +98,18 @@ public class LoginView extends AViewWomm {
                 notification.open();
             }
         });
+
+        loginFormLayout.add(
+                new H3(getWommBuilder().translateText("LogIn")),
+                usernameInput,
+                passwordInput,
+                submitButton
+        );
+
+        add(
+                new H1("w.o.o.m."),
+                loginFormLayout
+        );
     }
 
     @Override
