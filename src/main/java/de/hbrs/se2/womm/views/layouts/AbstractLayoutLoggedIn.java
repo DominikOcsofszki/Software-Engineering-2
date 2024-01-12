404bc1a566cea14f52420f6033d327aa844778af
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-12 15:46:45 +0100

    refactor(Menu): Extracting method into top AbstractLayoutLoggedIn

--- src/main/java/de/hbrs/se2/womm/views/layouts/AbstractLayoutLoggedIn.java
+++ src/main/java/de/hbrs/se2/womm/views/layouts/AbstractLayoutLoggedIn.java
@@ -1,6 +1,9 @@
 package de.hbrs.se2.womm.views.layouts;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.menubar.MenuBar;
 import de.hbrs.se2.womm.config.SecurityService;
 import de.hbrs.se2.womm.views.extra.VaadinBuilderWomm;
 
@@ -17,5 +20,15 @@ abstract class AbstractLayoutLoggedIn extends AbstractLayout {
 //        String text = VaadinBuilderWomm.translateTextStatic("Log out(") + nutzerId + "): " + username;
         Button logoutButton = new Button(text, e -> securityService.logout());
         super.createHeaderWithLogoutButton(logoutButton, true);
+    }
+
+    MenuBar setUpMenuBar(String name, Class<? extends Component> view) {
+        MenuBar menuBar = new MenuBar();
+        menuBar.setOpenOnHover(true);
+        menuBar.isOpenOnHover();
+        menuBar.setThemeName("blue");
+        menuBar.addItem(VaadinBuilderWomm.translateTextStatic(name),
+                e -> UI.getCurrent().navigate(view));
+        return menuBar;
     }
 }
--- src/main/java/de/hbrs/se2/womm/views/layouts/StudentLayout.java
+++ src/main/java/de/hbrs/se2/womm/views/layouts/StudentLayout.java
@@ -1,9 +1,7 @@
 package de.hbrs.se2.womm.views.layouts;
 
 import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.RouterLink;
 import de.hbrs.se2.womm.config.SecurityService;
@@ -33,15 +31,6 @@ public class StudentLayout extends AbstractLayoutLoggedIn {
 //        socialMediaSubMenu.addItem("Twitter", listener);
 //        socialMediaSubMenu.addItem("Instagram", listener);
 
-    MenuBar setUpMenuBar(String name, Class<? extends Component> view) {
-        MenuBar menuBar = new MenuBar();
-        menuBar.setOpenOnHover(true);
-        menuBar.isOpenOnHover();
-        menuBar.setThemeName("blue");
-        menuBar.addItem(VaadinBuilderWomm.translateTextStatic(name),
-                e -> UI.getCurrent().navigate(view));
-        return menuBar;
-    }
 
     @Override
     void createDrawer() {
