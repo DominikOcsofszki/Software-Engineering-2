9159c0b0d9c6bf38b010c4841d9245fee5b811ff
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-12 16:19:14 +0100

    refactor(Menu): Adding Icons to Menu and more options for changing. Extracting Method into AbstractLayoutLoggedIn

--- src/main/java/de/hbrs/se2/womm/views/layouts/AbstractLayoutLoggedIn.java
+++ src/main/java/de/hbrs/se2/womm/views/layouts/AbstractLayoutLoggedIn.java
@@ -3,7 +3,10 @@ package de.hbrs.se2.womm.views.layouts;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
 import de.hbrs.se2.womm.config.SecurityService;
 import de.hbrs.se2.womm.views.extra.VaadinBuilderWomm;
 
@@ -24,11 +27,21 @@ abstract class AbstractLayoutLoggedIn extends AbstractLayout {
 
     MenuBar setUpMenuBarWomm(String name, Class<? extends Component> view) {
         MenuBar menuBar = new MenuBar();
+        menuBar.addThemeVariants(MenuBarVariant.LUMO_ICON);
         menuBar.setOpenOnHover(true);
         menuBar.isOpenOnHover();
         menuBar.setThemeName("blue");
         menuBar.addItem(VaadinBuilderWomm.translateTextStatic(name),
                 e -> UI.getCurrent().navigate(view));
+        menuBar.setWidthFull();
+        return menuBar;
+    }
+
+    MenuBar setUpMenuBarWomm(String name, Class<? extends Component> view, VaadinIcon iconName) {
+        Icon icon = new Icon(iconName);
+        MenuBar menuBar = setUpMenuBarWomm(name, view);
+        menuBar.getChildren().findFirst().get().getElement().insertChild(0, icon.getElement());       //left side
+//        menuBar.getChildren().findFirst().get().getElement().insertChild(1, icon.getElement());     //TODO right side
         return menuBar;
     }
 }
--- src/main/java/de/hbrs/se2/womm/views/layouts/StudentLayout.java
+++ src/main/java/de/hbrs/se2/womm/views/layouts/StudentLayout.java
@@ -2,10 +2,9 @@ package de.hbrs.se2.womm.views.layouts;
 
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.RouterLink;
 import de.hbrs.se2.womm.config.SecurityService;
-import de.hbrs.se2.womm.views.extra.VaadinBuilderWomm;
 import de.hbrs.se2.womm.views.student.*;
 
 //@Component
@@ -35,13 +34,11 @@ public class StudentLayout extends AbstractLayoutLoggedIn {
     @Override
     void createDrawer() {
         addToDrawer(new VerticalLayout(
-                setUpMenuBarWomm("SAboStudentView", SAboStudentView.class),
-                new RouterLink(VaadinBuilderWomm.translateTextStatic("SAboStudentView"), SAboStudentView.class),
-                new RouterLink(VaadinBuilderWomm.translateTextStatic("SApplicationsView"), SApplicationsView.class),
-                new RouterLink(VaadinBuilderWomm.translateTextStatic("SCreateChangeStudentProfileView"), SCreateChangeStudentProfileView.class),
-                new RouterLink(VaadinBuilderWomm.translateTextStatic("SHomepageStudentView"), SHomepageStudentView.class),
-                new RouterLink(VaadinBuilderWomm.translateTextStatic("SStudentProfileDisplayView"), SStudentProfileDisplayView.class),
-                new RouterLink(VaadinBuilderWomm.translateTextStatic("SAboStudentView"), SAboStudentView.class)
+                setUpMenuBarWomm("SHomepageStudentView", SHomepageStudentView.class, VaadinIcon.HOME),
+                setUpMenuBarWomm("SStudentProfileDisplayView", SStudentProfileDisplayView.class, VaadinIcon.USER),
+                setUpMenuBarWomm("SAboStudentView", SAboStudentView.class, VaadinIcon.BOOKMARK),
+                setUpMenuBarWomm("SApplicationsView", SApplicationsView.class, VaadinIcon.BRIEFCASE),
+                setUpMenuBarWomm("SCreateChangeStudentProfileView", SCreateChangeStudentProfileView.class, VaadinIcon.EDIT)
 
   /* Old way deelte later after approve
    new RouterLink(VaadinBuilderWomm.translateTextStatic("SAboStudentView"), SAboStudentView.class),
