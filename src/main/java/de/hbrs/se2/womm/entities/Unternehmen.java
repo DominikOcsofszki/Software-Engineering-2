054616e44ae5636a0d2176cd17ee0815f7eb5432
Jan Kölzer <koelzerjan@gmail.com>
2024-01-13 16:18:29 +0100

    feat(Unternehmen): String für die Website URL

--- src/main/java/de/hbrs/se2/womm/entities/Unternehmen.java
+++ src/main/java/de/hbrs/se2/womm/entities/Unternehmen.java
@@ -26,6 +26,9 @@ public class Unternehmen {
     @Column(name = "unternehmen_gruendung")
     private String gruendung;
 
+    @Column(name = "unternehmen_website")
+    private String website_url;
+
     @OneToOne
     @JoinColumn(name = "nutzer_id")
     private Nutzer nutzer;
