1ca8ff568b339ebce07c4700a059286116c65a75
Jan Kölzer <koelzerjan@gmail.com>
2024-01-14 14:53:36 +0100

    fix(wrong Query):     At BenachrichtigungRepository

--- src/main/java/de/hbrs/se2/womm/repositories/BenachrichtigungRepository.java
+++ src/main/java/de/hbrs/se2/womm/repositories/BenachrichtigungRepository.java
@@ -2,11 +2,10 @@ package de.hbrs.se2.womm.repositories;
 
 import de.hbrs.se2.womm.entities.Benachrichtigung;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface BenachrichtigungRepository extends JpaRepository<Benachrichtigung, Long> {
-    List<Benachrichtigung> findByNutzer_id(Long id);
+    List<Benachrichtigung> findBenachrichtigungByNutzer_NutzerId(Long id);
+    List<Benachrichtigung> findBenachrichtigungByNutzer_NutzerIdAndGelesenIsFalse(Long id);
 }
