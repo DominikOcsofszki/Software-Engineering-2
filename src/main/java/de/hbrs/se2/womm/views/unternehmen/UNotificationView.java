bbde5665f16e8412911a1449fb69b58de03f6156
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-17 11:05:31 +0100

    feat(schat,unotification): remove counter for new messages after going to the page

--- src/main/java/de/hbrs/se2/womm/views/unternehmen/UNotificationView.java
+++ src/main/java/de/hbrs/se2/womm/views/unternehmen/UNotificationView.java
@@ -5,12 +5,15 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hbrs.se2.womm.config.SecurityService;
+import de.hbrs.se2.womm.dtos.BenachrichtigungDTO;
 import de.hbrs.se2.womm.services.BenachrichtigungService;
 import de.hbrs.se2.womm.views.components.GridFilterMessages;
 import de.hbrs.se2.womm.views.layouts.AViewWomm;
 import de.hbrs.se2.womm.views.layouts.ROUTING;
 import de.hbrs.se2.womm.views.layouts.UnternehmenLayout;
 import jakarta.annotation.security.RolesAllowed;
+
+import java.util.List;
 
 @Route(value = ROUTING.UNTERNEHMEN.UNotificationView, layout = UnternehmenLayout.class)
 @RolesAllowed({"UNTERNEHMEN","ADMIN"})
@@ -25,7 +28,12 @@ public class UNotificationView extends AViewWomm {
         this.benachrichtigungService = benachrichtigungService;
         this.securityService = securityService;
         setUpHeader();
-        gridFilterMessages.setUpFromOutside(benachrichtigungService.getByNutzerId(securityService.getLoggedInNutzerID()));
+        List<BenachrichtigungDTO> msgByNutzerId = benachrichtigungService.getByNutzerId(securityService.getLoggedInNutzerID());
+        gridFilterMessages.setUpFromOutside(msgByNutzerId);
+        msgByNutzerId.forEach(benachrichtigungDTO -> {
+            benachrichtigungDTO.setGelesen(true);
+            benachrichtigungService.saveBenachrichtigung(benachrichtigungDTO);
+        });
         add(gridFilterMessages);
     }
 
